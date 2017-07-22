ee43435f86cbe0661ccd626e8dd2fdfd5b8eb210
Parwinder Bhagat <singhparwinder@me.com>
2017-07-22 06:45:59 -0500

    Added getRepos and respective template

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -2,16 +2,20 @@ import { GithubService } from './../services/github.service';
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    selector: 'profile',
-    templateUrl: 'profile.component.html'
+	moduleId: module.id,
+	selector: 'profile',
+	templateUrl: 'profile.component.html'
 })
-
 export class ProfileComponent {
-    user:Object;
-    constructor(private _githubService:GithubService){
-        this._githubService.getUser().subscribe(user => {
-            this.user = user;
-        })
-    }
+	user: Object;
+	repos: any[];
+	constructor(private _githubService: GithubService) {
+		this._githubService.getUser().subscribe(user => {
+			this.user = user;
+		});
+
+		this._githubService.getRepos().subscribe(repos => {
+			this.repos = repos;
+		});
+	}
 }
--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -4,20 +4,40 @@ import 'rxjs/add/operator/map';
 import { KeyService } from './../config/config';
 
 @Injectable()
-export class GithubService{
-    private username:string;
-    private client_id:string;
-    private client_secret: string;
+export class GithubService {
+	private username: string;
+	private client_id: string;
+	private client_secret: string;
 
-    constructor(private _http:Http, private config:KeyService){
-        console.log('GitHub Service Ready!');
-        this.username = 'bhagatparwinder';
-        this.client_id = config.getKey('client_id');
-        this.client_secret = config.getKey('client_secret');
-    }
+	constructor(private _http: Http, private config: KeyService) {
+		this.username = 'bhagatparwinder';
+		this.client_id = config.getKey('client_id');
+		this.client_secret = config.getKey('client_secret');
+	}
 
-    getUser(){
-        return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
-            .map(res => res.json());
-    }
+	getUser() {
+		return this._http
+			.get(
+				'http://api.github.com/users/' +
+					this.username +
+					'?client_id=' +
+					this.client_id +
+					'&client_secret=' +
+					this.client_secret
+			)
+			.map(res => res.json());
+	}
+
+	getRepos() {
+		return this._http
+			.get(
+				'http://api.github.com/users/' +
+					this.username +
+					'/repos?client_id=' +
+					this.client_id +
+					'&client_secret=' +
+					this.client_secret
+			)
+			.map(res => res.json());
+	}
 }
